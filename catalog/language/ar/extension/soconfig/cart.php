aea7099218735b13e3b4c42a9b25bfd2fdc1f0fc
Süleyman Ekici <info@suleymanekici.com.tr>
2019-01-28 01:28:14 +0300

    a

--- catalog/language/ar/extension/soconfig/cart.php
+++ catalog/language/ar/extension/soconfig/cart.php
@@ -5,7 +5,7 @@ $_['heading_title']          = 'سلة';
 // Text
 $_['text_title']             = 'وأضاف المنتج إلى السلة';
 $_['text_thumb']             = '<img src="%s" alt="" />';
-$_['text_success']           = '<a href="%s"> <a href="%s"> added </a> to shopping cart% s </a>!';
+$_['text_success']           = '<a href="%s"> <a href="%s"> وأضاف </a> الى عربة التسوق %s </a>!';
 $_['text_items']     	    = '<span class="items_cart">%s </span><span class="items_cart">المنتجات</span><span class="items_carts"> - %s</span>';
 $_['text_shop']  			  = 'تسوق الآن';
 $_['text_shop_cart']		  = 'سلتي';
