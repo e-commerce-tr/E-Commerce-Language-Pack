0fc6d90f48d8ddc41726b703aba3fc44038661ad
Süleyman Ekici <info@suleymanekici.com.tr>
2019-01-29 00:47:55 +0300

    ispanyolca çeviri tamamlandı v2
    developer süleyman ekici

--- catalog/language/es-es/extension/module/simple_blog.php
+++ catalog/language/es-es/extension/module/simple_blog.php
@@ -6,7 +6,7 @@ $_['text_date_format'] = '<b> d </ b> M';
 $_['text_popular_all'] = 'Artículos populares';
 $_['text_latest_all'] = 'Mensajes recientes';
 $_['text_no_result'] = '¡No hay resultados!';
-$_['text_comments'] = '& nbsp; comentarios';
-$_['text_comment'] = '& nbsp; Comentario';
+$_['text_comments'] = '&nbsp; comentarios';
+$_['text_comment'] = '&nbsp; Comentario';
 $_['show_all_text'] = 'Ver todo';
  
